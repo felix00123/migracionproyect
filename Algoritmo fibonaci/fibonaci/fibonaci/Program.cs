be85247aed6a01def58ae48ed5fea890e89680cb
felix manuel rojas guzman <manuel00fx@hotmail.com>
2021-07-29 21:26:34 -0400

    fibonaci realizado

--- Algoritmo fibonaci/fibonaci/fibonaci/Program.cs
+++ Algoritmo fibonaci/fibonaci/fibonaci/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace fibonaci
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int hey;
+
+
+            try
+            {
+                Console.Write("por favor ingrese y le buscaremos " +
+                    "los 5 fibonaci siguientes:");
+                
+                //Verificamos si se ha ingresado un numero
+                if (int.TryParse(Console.ReadLine(), out hey))
+                {
+                    fibonaci5.print5fibonaci(hey);
+                }
+                else
+                {
+
+                    Console.WriteLine("numero invalido");
+
+                }
+
+            }
+            catch (Exception E)
+            {
+
+                Console.WriteLine(" "+E.Message);
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
---
+++ Algoritmo fibonaci/fibonaci/fibonaci/fibonaci5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace fibonaci
+{
+    public static class fibonaci5
+    {
+        public static void  print5fibonaci(int num) {
+
+            int num2 = num - 1;
+            int result = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                result = num + num2;
+                Console.WriteLine(result );
+                  num2=num;
+                  num = result;
+                 result = 0;
+                
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
